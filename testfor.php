5cedaf6b97981d4c3e9762fb16fa57d4ff574209
和祯_BT <2821721063@qq.com>
2022-08-10 22:23:53 +0800

    6

--- testfor.php
+++ testfor.php
@@ -154,6 +154,7 @@ switch ($type)
     {
     if (!isset($_COOKIE["submitcode"]))
     {
+        echo $_COOKIE["submitcode"];
         echo '<a class=login style="height:370px;text-decoration:none" href="javascript:window.opener=null;window.close();"><h1 style="color:rgb(215, 215, 215)"><br><br>验证码过期啦，重新提交吧<br><br><svg width="100px" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
         <path d="M256 8C119 8 8 119 8 256s111 248 248 248 248-111 248-248S393 8 256 8zm0 448c-110.5 0-200-89.5-200-200S145.5 56 256 56s200 89.5 200 200-89.5 200-200 200zm101.8-262.2L295.6 256l62.2 62.2c4.7 4.7 4.7 12.3 0 17l-22.6 22.6c-4.7 4.7-12.3 4.7-17 0L256 295.6l-62.2 62.2c-4.7 4.7-12.3 4.7-17 0l-22.6-22.6c-4.7-4.7-4.7-12.3 0-17l62.2-62.2-62.2-62.2c-4.7-4.7-4.7-12.3 0-17l22.6-22.6c4.7-4.7 12.3-4.7 17 0l62.2 62.2 62.2-62.2c4.7-4.7 12.3-4.7 17 0l22.6 22.6c4.7 4.7 4.7 12.3 0 17z"></path>
         </svg><br></h1></a>';
@@ -162,7 +163,6 @@ switch ($type)
     {
         $key=$_POST["submitkey"];
         $code=$_SESSION['submitcode'];
-        echo $_SESSION['submitcode'];
         if ($code==$key)
         {
             $author=$_SESSION['name'];
