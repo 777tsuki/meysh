82da149c631f836d2dbb03082ef6c068d887c1bd
和祯_BT <2821721063@qq.com>
2022-08-22 20:47:05 +0800

    6

--- user.php
+++ user.php
@@ -95,9 +95,9 @@ include 'table.php';
               $redetect->bindValue(':mail', $mail, PDO::PARAM_STR);
               $redetect->execute();
               $result2 = $redetect->fetch(PDO::FETCH_ASSOC);
-              if ($mail=="$result1[mail]")
+              if ("$mail"=="$result1[mail]")
               {$result=1;}
-              elseif ($mail=="$result2[mail]")
+              elseif ("$mail"=="$result2[mail]")
               {$result=-1;}
               else
               {$result=0;}
