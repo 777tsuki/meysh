f5876f07f3218636abcf4c2bc332b1fb6d2547d2
和祯_BT <2821721063@qq.com>
2022-08-23 20:45:42 +0800

    6

--- index.php
+++ index.php
@@ -5,7 +5,7 @@ include 'table.php';
 ?>
 <html>
 <head>
-<link rel="shortcut icon" href="source/icon/logo.png">
+<link rel="shortcut icon" href="source/icon/icon.png">
 <title>梅什号 | 首页</title>
 <meta name="description" content="梅什号">
 <meta name="keywords" content="nothing">
@@ -24,16 +24,16 @@ readfile("source/menus.html");
 echo $normalmenu;
 ?>
 <div class="row">
-  <img class="blank1" src="source/icon/blank.png" width="44" height="1000">
   <div class="main">
-    <img class="headlineimg" src="source/img/base/background.webp"></a><p></p>
     <div class="tips">
+      <img class="headlineimg" src="source/img/base/meysh.webp"></a>
+      <div class="headlineright" style="width:37.9%;height:517px"><h2>aye</h2><hr/></div>
+      <div class="blankdiv"><h9>6666666666666666666666666666</h9></div>
       <div class="headlineleft"><h2>最新公告</h2><hr/><iframe class="iframe" loading="lazy" src="" width="100%" frameborder="0" height="100%"></iframe></div><p class="blankp"></p>
-      <div class="headlineright"><h2>最新航线</h2><hr/><iframe class="iframe" loading="lazy" src="" width="100%" frameborder="0" height="100%"></iframe></div>
-      <div class="blankdiv"><h9>666</h9></div>
-      <div class="headlineleft"><h2>最新话题</h2><hr/><iframe class="iframe" loading="lazy" src="" width="100%" frameborder="0" height="100%"></iframe></div><p class="blankp"></p>
-      <div class="headlineright"><h2>最新故事</h2><hr/><iframe class="iframe" loading="lazy" src="" width="100%" frameborder="0" height="100%"></iframe></div>
-      <div class="blankdiv"><h9>666</h9></div>
+      <!--div class="headlineright"><h2>最新航线</h2><hr/><iframe class="iframe" loading="lazy" src="" width="100%" frameborder="0" height="100%"></iframe></div-->
+      <div class="headlineright"><h2>最新话题</h2><hr/><iframe class="iframe" loading="lazy" src="" width="100%" frameborder="0" height="100%"></iframe></div><p class="blankp"></p>
+      <!--div class="headlineright"><h2>最新故事</h2><hr/><iframe class="iframe" loading="lazy" src="" width="100%" frameborder="0" height="100%"></iframe></div-->
+      <div class="blankdiv"><h9>6666666666666666666666666666</h9></div>
       <div class="headline"><h2>相关链接</h2><hr/><a href="https://masiro.me/" target="_blank"><img src="https://www.masiro.me/masiroImg/logo-small.ico" width="40px"><p></p></a></div>
     </div>
   </div>
